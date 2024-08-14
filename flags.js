770af4716ee9a0d68b302f6a1bfe695e90f3c761
German David Perez Morales <yanger@allosaurus.local>
2024-08-14 12:42:07 -0600

    Primer comit con pocas banderas

---
+++ flags.js
@@ -0,0 +1,51 @@
+// banderas.js
+
+// Array of flag objects with their URLs and names
+const flags = [
+    { url: './flags/al.webp', name: 'Albania'},
+    { url: './flags/af.webp', name: 'Afganistán'},
+    { url: './flags/ax.webp', name: 'Islas Åland'},
+    { url: './flags/de.webp', name: 'Alemania'},
+    { url: './flags/ad.webp', name: 'Andorra'},
+    { url: './flags/ao.webp', name: 'Angola'},
+    { url: './flags/ai.webp', name: 'Anguila'},
+    { url: './flags/aq.webp', name: 'Antártida'},
+    { url: './flags/ag.webp', name: 'Antigua y Barbuda'},
+    { url: './flags/sa.webp', name: 'Arabia Saudita'},
+    { url: './flags/dz.webp', name: 'Argelia'},
+    { url: './flags/ar.webp', name: 'Argentina'},
+    { url: './flags/am.webp', name: 'Armenia'},
+    { url: './flags/aw.webp', name: 'Aruba'},
+    { url: './flags/au.webp', name: 'Australia'},
+    { url: './flags/at.webp', name: 'Austria'},
+    { url: './flags/az.webp', name: 'Azerbaiyán'},
+    { url: './flags/bs.webp', name: 'Bahamas'},
+    { url: './flags/bh.webp', name: 'Baréin'},
+    { url: './flags/bd.webp', name: 'Bangladesh'},
+    { url: './flags/bb.webp', name: 'Barbados'},
+    { url: './flags/be.webp', name: 'Bélgica'},
+    { url: './flags/bz.webp', name: 'Belice'},
+    { url: './flags/bj.webp', name: 'Benín'},
+    { url: './flags/bm.webp', name: 'Bermudas'},
+    { url: './flags/by.webp', name: 'Bielorrusia'},
+    { url: './flags/bo.webp', name: 'Bolivia'},
+    { url: './flags/ba.webp', name: 'Bosnia y Herzegovina'},
+    { url: './flags/bw.webp', name: 'Botsuana'},
+    { url: './flags/br.webp', name: 'Brasil'},
+    { url: './flags/bn.webp', name: 'Brunéi'},
+    { url: './flags/bg.webp', name: 'Bulgaria'},
+    { url: './flags/bf.webp', name: 'Burkina Faso'},
+    { url: './flags/bi.webp', name: 'Burundi'},
+    { url: './flags/bt.webp', name: 'Bután'},
+    { url: './flags/cv.webp', name: 'Cabo Verde'},
+    { url: './flags/kh.webp', name: 'Camboya'},
+    { url: './flags/cm.webp', name: 'Camerún'},
+    { url: './flags/ca.webp', name: 'Canadá'},
+    { url: './flags/bq.webp', name: 'Caribe Neerlandés'}
+    
+
+    // Add more flags as needed
+];
+
+// Export the flags array for use in other scripts
+export { flags };
